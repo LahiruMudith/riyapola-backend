3e13b890249e18f53a977257a3e21b57307e208e
LahiruMudith <lahimudith@gmail.com>
2024-03-02 10:33:00 +0530

    add core npm code that

--- index.js
+++ index.js
@@ -1,10 +1,20 @@
 const express = require('express')
+const cors = require('cors');
 var bodyParser = require('body-parser')
 const app = express()
 const port = 3000;
 const customerRoutes = require("./routes/customer-route")
 const adminRoutes = require("./routes/admin-route");
 const vehicleRoutes = require("./routes/vehicle-route");
+
+const corsOptions = {
+  origin: 'http://localhost:5173',
+  methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
+  credentials: true,
+  optionsSuccessStatus: 204,
+};
+
+app.use(cors(corsOptions));
 
 // parse application/x-www-form-urlencoded
 app.use(bodyParser.urlencoded({ extended: false }))
