46483777fde491e587c7a185b6621682be53bef5
LahiruMudith <lahimudith@gmail.com>
2024-03-12 00:42:46 +0530

    jwt adding

--- controllers/customer-controller.js
+++ controllers/customer-controller.js
@@ -1,9 +1,12 @@
 const connection = require('../db/db-connection');
+// var { expressjwt: jwt } = require("express-jwt");
+var jwt = require('jsonwebtoken');
+
 
 const saveCustomer = (req, res) => {
-    connection.query('insert into customer values(?,?,?,?,?)',[req.body.name, req.body.nic, req.body.address, req.body.contactNumber, req.body.password], (err, rows) => {
+    connection.query('insert into customer values(?,?,?,?,?,?)',[req.body.name, req.body.nic, req.body.address, req.body.contactNumber, req.body.password, req.body.contactNumber+req.body.nic+req.body.password], (err, rows) => {
         if (err) throw err
-        res.send(rows)
+        res.send("Token :" + req.body.contactNumber+req.body.nic+req.body.password)
       })
 }
 const updateCustomer = (req, res) => {
@@ -18,5 +21,18 @@ const deleteCustomer = (req, res) => {
       res.send(rows)
     })
 }
+const loginCustomer = (req, res) => {
+  const user = {id:3};
+  const token = jwt.sign({user}, 'my_secrect_key');
+  connection.query('select NIC_number,password from customer where NIC_number=? && password=? ',[req.body.nic_number, req.body.password], (err, rows) => {
+      if (err) throw err
+      if (rows.length > 0) {
+        res.send("Token: " + token)
+      }else {
+        res.send("NIC or Password Incorrect")
+      }
+      
+    })
+}
 
-module.exports = {saveCustomer,updateCustomer,deleteCustomer}
+module.exports = {saveCustomer,updateCustomer,deleteCustomer, loginCustomer}
--- routes/customer-route.js
+++ routes/customer-route.js
@@ -1,9 +1,10 @@
 const express = require('express')
 const router = express.Router();
-const {saveCustomer, updateCustomer, deleteCustomer} = require("../controllers/customer-controller");
+const {saveCustomer, updateCustomer, deleteCustomer, loginCustomer} = require("../controllers/customer-controller");
 
 router.post('/add', saveCustomer );
 router.put('/update/:updateNic', updateCustomer);
 router.delete('/delete/:deleteNic', deleteCustomer);
+router.get('/login', loginCustomer);
 
 module.exports =  router;
